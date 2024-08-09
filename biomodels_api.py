cfdd0c5060f97546e6a302341b07782c10059bc1
prkraa002 <PRKRAA002@myuct.ac.za>
2024-08-09 18:13:22 +0200

    Updated biomodels api

--- biomodels_api.py
+++ biomodels_api.py
@@ -12,3 +12,22 @@ print(submissionId, publicationId)
 
 # biomodels api gives header of SBML file and other info as above
 # cannot actually download
+
+'''
+# actual biomodels api - downloads file into cache, (can read from cache - unkown path config)
+
+import biomodels
+
+for i in range(1, 10):
+
+    metadata = biomodels.get_metadata(f"BIOMD00000000{i:02d}")
+    # print(metadata) # view all files from model
+
+    biomodels.get_file(f"BIOMD00000000{i:02d}_url.xml", model_id=f"BIOMD{i}")
+    path = biomodels.get_file(metadata[0])
+    
+    print(path.read_text())
+
+
+
+'''

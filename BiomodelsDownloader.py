51e412d3edc7c101533073a7e4250e7572456c11
prkraa002 <PRKRAA002@myuct.ac.za>
2024-08-28 14:10:23 +0200

    Fully functional pipeline with biomodels database, can now verify that all models are downloaded and the latests models are added. Server has some problems with few models, they have been manually removed

--- BiomodelsDownloader.py
+++ BiomodelsDownloader.py
@@ -3,7 +3,6 @@ import zipfile
 import io
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from biomodels_restful_api_client import services as bmservices
 
 class BiomodelsDownloader:
     """
@@ -12,12 +11,12 @@ class BiomodelsDownloader:
     This can be done in parallel depending on the number of threads allocated
     """
 
-    def __init__(self, base_url="https://www.ebi.ac.uk/biomodels/search/download", num_models=50, threads=10, output_dir="biomodels", curatedOnly=True):
+    def __init__(self, base_url="https://www.ebi.ac.uk/biomodels/search/download", num_models=-1, threads=10, output_dir="biomodels", curatedOnly=True):
         """
         Initialize the downloader with the base URL and configuration for downloading.
 
         base_url (str): The base URL for downloading the models. Default is "https://www.ebi.ac.uk/biomodels/search/download".
-        num_models (int): The number of models to download. Default is 50.
+        num_models (int): The number of models to download. Default is -1 = all models. This will be changed to length of curated models.
         threads (int): The number of threads to use for parallel downloading. Default is 10.
         output_dir (str): The directory where the downloaded models will be stored. Default is "biomodels".
         curatedOnly (bool):Only adds curated models to the database.  
@@ -28,10 +27,22 @@ class BiomodelsDownloader:
         self.max_workers = threads
         self.output_dir = output_dir
         self.curatedOnly = curatedOnly
+        self.curated_models = []
+        self.uncurated_models = []
+
+        # Query biomodels for available models
+        self.check_available_models()
+
+        # If a number of models is not specifified, it is presumed to be all
+        # handles whether uncurated models are included
+        if self.num_models == -1:
+            self.num_models = len(self.curated_models) if self.curatedOnly else (len(self.curated_models) + len(self.uncurated_models))
 
         # Ensure the output directory exists
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
+
+        
 
     def download_and_extract(self, model_id):
         """
@@ -43,25 +54,22 @@ class BiomodelsDownloader:
         Returns:
             bool: True if the download and extraction were successful, False otherwise.
         """
-
-        if self.curatedOnly:
-            if not self.check_curation_status(model_id):
-                print(model_id, ": not curated")
-                return False
-
+        
         url = f"{self.base_url}?models={model_id}"
         response = requests.get(url)
         
         # OK response
         if response.status_code == 200:
-
+            
             zip_file = zipfile.ZipFile(io.BytesIO(response.content))
+            
+            # Extract xml from downloaded zip file
             for file_info in zip_file.infolist():
-
+                
                 if file_info.filename.endswith('.xml'):
                     xml_content = zip_file.read(file_info.filename)
                     output_path = os.path.join(self.output_dir, file_info.filename)
-                    
+
                     with open(output_path, 'wb') as xml_file:
                         xml_file.write(xml_content)
                     print(f"Extracted {file_info.filename} to {self.output_dir}")
@@ -78,31 +86,78 @@ class BiomodelsDownloader:
         """
         Start the download and extraction process for all models in parallel.
         """
+        downloadable_models = []
+
+        if self.curatedOnly:
+            downloadable_models = self.curated_models
+        else:
+            downloadable_models = self.curated_models + self.uncurated_models
+
         with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
 
-            futures = [executor.submit(self.download_and_extract, f"BIOMD{i:010}") 
-                       for i in range(1, self.num_models)]
+            futures = [executor.submit(self.download_and_extract, downloadable_models[i]) 
+                       for i in range(0, self.num_models)]
             
             for future in as_completed(futures):
-                future.result()  # Re-raise any exception that was caught during execution
+                future.result()  # Re-raise any Unsuccesful Download that was caught during execution
     
 
-    def check_exist_and_up_to_date(self): # TODO: check if biomodel exists and latest version
-        pass
+    def verifiy_models(self): # TODO: check if biomodel exists and latest version
+        
+        self.check_available_models()
+        
+        damaged_lost_models = []
+        path = self.output_dir
+        print(len(self.curated_models))
 
-    def check_curation_status(self, model_id): # TODO: check if biomodel is curated 
+        for model in self.curated_models:
+
+            model_file = f"{path}/{model}.xml"
+            if not os.path.isfile(model_file):
+                print(model)
+                damaged_lost_models.append(model)
+
+        if len(damaged_lost_models) == 0:
+            print("All models up to date")
+            return 
+
+        for model in damaged_lost_models:
+            self.download_and_extract(model)
+
+
+    def check_available_models(self):
         """
-        Queries the restful biomodels api and returns curation status of selected model
+        Queries the restful biomodels api and returns a list of all curated and non curated models 
         """
 
-        model = bmservices.get_model_info(model_id)
-        curationStatus = model['curationStatus']
-        return curationStatus
+        url = "https://www.ebi.ac.uk/biomodels/model/identifiers?format=json"
+        response = requests.get(url).json()
+
+        # Extract list of models from json 
+        model_names = response["models"]
+
+        # Split models into curated and uncurated using list comprehensions 
+        # All curated models are prefixed with BIOMD
+        # All non-curated models are prefixed with MODEL
+        self.curated_models = [model for model in model_names if model.startswith("BIOMD")]
+        self.uncurated_models = [model for model in model_names if model.startswith("MODEL")]
+
+        # SERVER ISSUES WITH FEW PROBLAMATIC MODELS - can be removed if resolved
+        problematic_models = ["BIOMD0000001069", "BIOMD0000001075", "BIOMD0000001066", "BIOMD0000001067", "BIOMD0000001068", "BIOMD0000001070", 
+                              "BIOMD0000001071", "BIOMD0000001073", "BIOMD0000001074", "BIOMD0000001076"]
+        
+        for p in problematic_models:
+            self.curated_models.remove(p)
+
+       
+        
     
-
-# Usage this is an example of how the doanloader should be called
+# Usage this is an example of how the downloader should be called
 
 if __name__ == "__main__":
 
-    downloader = BiomodelsDownloader(num_models=50, threads=10, curatedOnly=True)
-    downloader.run()
+    downloader = BiomodelsDownloader(threads=10, curatedOnly=True)
+    # This can be a select models, and the run can be a confirm -- unless that can be done by gui exclusively 
+    # then this should be done in one step 
+    # downloader.run()
+    downloader.verifiy_models()
